7f0c62612b6e5846f6c3a72e8e7436540ec46100
Gencebay <gencebay.demir@gmail.com>
2015-06-27 22:05:28 +0000

    Express 4 Migration

--- server.js
+++ server.js
@@ -1,31 +1,54 @@
-/**
- * Module dependencies.
- */
+var http = require('http');
+var express = require('express');
+var routes = require('./app/routes');
+var path = require('path');
 
-var express = require('express')
-  , routes = require('./routes')
-  , http = require('http');
+var logger = require('morgan');
+var methodOverride = require('method-override');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var multer = require('multer');
+var errorHandler = require('errorhandler');
+var expressHbs = require("express-handlebars");
+
+var mongoose = require("mongoose");
 
 var app = express();
-var server = app.listen(3000);
-// var io = require('socket.io').listen(server); // this tells socket.io to use our express server
 
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'handlebars');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.static(__dirname + '/public'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+
+app.engine('hbs', expressHbs({extname:'hbs', defaultLayout:'main.hbs'}));
+app.set('view engine', 'hbs');
+app.use(logger('dev'));
+app.use(methodOverride());
+app.use(session({ resave: true,
+                  saveUninitialized: true,
+                  secret: 'uwotm8' }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(multer());
+app.use(express.static(path.join(__dirname, 'public')));
+
+routes.initialize(app);
+
+// error handling middleware should be loaded after the loading the routes
+if ('development' == app.get('env')) {
+  app.use(errorHandler());
+}
+
+//connect to the db server:
+mongoose.connect('mongodb://localhost/test');
+mongoose.connection.on('open', function() {
+    console.log("Connected to Mongoose...");
+
+    // check if the db is empty, if so seed it with some contacts:
+    // seeder.check();
 });
 
-app.configure('development', function(){
-  app.use(express.errorHandler());
+
+var server = http.createServer(app);
+server.listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
 });
-
-app.get('/', routes.index);
-
-
-console.log("Express server listening on port 3000");
