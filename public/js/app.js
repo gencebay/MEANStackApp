57beda1d523b1326e128775b3a06f6f3b7cb6f4a
Gencebay <gencebay.demir@gmail.com>
2015-06-28 21:54:54 +0000

    angular settings, sample todo

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,24 @@
 var demoApp = angular.module('demoApp', []);
 
-demoApp.controller('mainController', function(){
+demoApp.controller('mainController', function($scope, $http) {
+    $scope.formData = {};
     
+    $http.get('/api/todos').success(function(data) {
+        $scope.todos = data;
+        console.log(data);
+    }).error(function(data) {
+        console.error(data);
+    });
+    
+    $scope.createTodo = function () {
+        $http.post('/api/todos', $scope.formData)
+            .success(function (data) {
+                $scope.formData = {};
+                $scope.todos = data;
+                console.log(data);
+            })
+            .error(function(data) {
+                console.error(data);
+            });
+    };
 });
